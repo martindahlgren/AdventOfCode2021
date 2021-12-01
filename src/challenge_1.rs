e8f787488211c64ca9ecd282090dbcd2ef491a49
Martin Dahlgren <18004887+martindahlgren@users.noreply.github.com>
2021-12-01 21:57:46 +0100

    Day 1 challenge 2

--- src/challenge_1.rs
+++ src/challenge_1.rs
@@ -3,12 +3,19 @@ use crate::common;
 pub fn challenge1() {
     println!("First Advent. Challenge 1");
 
-    let nincreases = get_values().collect::<Vec<_>>().windows(2).filter(|w| w[0] < w[1]).count();
+    let values = get_values().collect::<Vec<_>>();
+    let nincreases = number_of_increases(&values);
     println!("There are {} positive steps", nincreases);
 
+    println!("First Advent. Challenge 2");
+    let moving_sum: Vec<i16> = values.windows(3).map(|w| w[0] + w[1] + w[2]).collect();
+    let nincreases_three_summed = number_of_increases(&moving_sum);
+    println!("There are {} positive steps when making a moving average(/sum)", nincreases_three_summed);
 
+}
 
-
+fn number_of_increases(input: &Vec<i16>) -> usize {
+    input.windows(2).filter(|w| w[0] < w[1]).count()
 }
 
 fn get_values() -> impl Iterator<Item = i16> {
